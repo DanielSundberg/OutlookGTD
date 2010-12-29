41c16882385b03de37322f6351d3d86b6fe9c0ff
Daniel Sundberg <sumpan@gmail.com>
2010-12-29 16:43:10 +0100

    -Add project to hold the UI components

---
+++ OutlookGTD/OutlookGTP.UI/TaskGTDView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OutlookGTD.UI
+{
+    public partial class TaskGTDView : UserControl
+    {
+        public TaskGTDView()
+        {
+            InitializeComponent();
+        }
+    }
+}
